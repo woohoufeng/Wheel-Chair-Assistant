b4fc17209604d192fddd9cbfd425d815705dd5c7
woohoufeng <86917848+woohoufeng@users.noreply.github.com>
2021-08-20 18:42:54 +0800

    Add files via upload

---
+++ csvfile.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS csvfile (
+    `FirstName` VARCHAR(5) CHARACTER SET utf8,
+    `LastName` VARCHAR(1) CHARACTER SET utf8,
+    `City` VARCHAR(2) CHARACTER SET utf8,
+    `Age` INT
+);
+INSERT INTO csvfile VALUES
+    ('John','K','SB',22),
+    ('Stacy','H','LV',34),
+    ('Jack','V','LA',53);
